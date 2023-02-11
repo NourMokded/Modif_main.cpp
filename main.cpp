7f498fc99e4aab344a41af940f5dc821d7731f50
mouadhgit <mouadhdahech2@gmail.com>
2023-02-11 22:41:12 +0100

    print hello c mouadh

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file main.cpp
+ * @author Muaadh Dahech (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-02-11
+ * 
+ * @copyright Copyright (c) 2023, CRI
+ * 
+ */
+
+#include <iostream>
+
+int main(){
+
+    std::cout <<"Hello c mouadh"<<std::endl;
+    /*Please Write Hello c nour*/
+}
